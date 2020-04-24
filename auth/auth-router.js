4e38bc7521c3aa1dfccdf5609c8c3536f9990e32
Gregory Hawman <g.hawman@gmail.com>
2020-04-24 08:46:24 -0700

    got my login up, need to write the middleware though

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,22 +7,41 @@ const { rounds } = require('../api/secrets');
 const Users = require('./auth-models');
 
 router.post('/register', (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, rounds);
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, rounds);
 
-  user.password = hash;
+    user.password = hash;
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'Cannot add user', error });
-    });
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Cannot add user', error });
+      });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ 
+          message: `Welcome ${user.username}!`,
+          token: token, 
+        });
+      } else {
+        res.status(401).json({ 
+          message: 'You shall not pass! (Login)' 
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: error.message });
+    });
 });
 
 module.exports = router;
