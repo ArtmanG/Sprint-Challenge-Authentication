b8453f529c25ffd3ca680b72e7b61b22967c2753
Gregory Hawman <g.hawman@gmail.com>
2020-04-24 08:42:44 -0700

    got my register up

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { generateToken } = require('../middleware/middleware');
+const { rounds } = require('../api/secrets');
+const Users = require('./auth-models');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, rounds);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Cannot add user', error });
+    });
 });
 
 router.post('/login', (req, res) => {
